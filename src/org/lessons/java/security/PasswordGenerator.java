2f60d7bb77aac716600f095bdc4606599565f1c1
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-12 14:03:21 +0100

    first commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+public static void main(String[] args) {
+    String nome = "Aldo";
+    String cognome = "Baglio";
+    String colore = "Marrone";
+    int giorno = 24; 
+    int mese = 9;
+    int anno = 1963;
+    int somma = giorno + mese + anno;
+    System.out.println( "la tua password è " + nome + "-" + cognome + "-" +  colore + "-" + somma);;
+}
+}

2a89c7ab3cdb51973d8d81d8b2d8e9547108a7d6
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-12 14:17:09 +0100

    feat: creata la generazione di password ad input

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,10 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 public static void main(String[] args) {
+    // creazione automatica password
     String nome = "Aldo";
     String cognome = "Baglio";
     String colore = "Marrone";
@@ -9,6 +12,25 @@ public static void main(String[] args) {
     int mese = 9;
     int anno = 1985;
     int somma = giorno + mese + anno;
-    System.out.println( "la tua password è " + nome + "-" + cognome + "-" +  colore + "-" + somma);;
+    System.out.println( "la tua password è " + nome + "-" + cognome + "-" +  colore + "-" + somma);
+// creazione password con input utente
+Scanner in = new Scanner(System.in); 
+System.out.print("inserisci il nome");
+ String nomeUtente = in.next();
+ System.out.print("inserisci il cognome");
+ String cognomeUtente = in.next();
+ System.out.print("inserisci il colore preferito");
+ String coloreUtente = in.next();
+ System.out.print("inserisci il giorno di nascita");
+ int giornoUtente = in.nextInt();
+ System.out.print("inserisci il mese dinascita");
+ int meseUtente = in.nextInt();
+ System.out.print("inserisci anno di nascita");
+ int annoUtente = in.nextInt();
+
+ int sommaUtente = giornoUtente + meseUtente + annoUtente;
+
+System.out.print("la tua password è " + nomeUtente + "-" + cognomeUtente + "-" +  coloreUtente + "-" + sommaUtente);
+
 }
 }

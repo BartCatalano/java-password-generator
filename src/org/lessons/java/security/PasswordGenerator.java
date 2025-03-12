f4aa3b51994ba8f60f84c4fe2a5a570ac97a58f5
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-12 14:04:14 +0100

    feat: prova push

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
     String colore = "Marrone";
     int giorno = 24; 
     int mese = 9;
-    int anno = 1963;
+    int anno = 1985;
     int somma = giorno + mese + anno;
     System.out.println( "la tua password è " + nome + "-" + cognome + "-" +  colore + "-" + somma);;
 }
